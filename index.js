9a47ce02239991f0882ac4d223fc3a3449a19bbd
Javier Lona <javierlona1@gmail.com>
2019-12-26 14:23:55 -0600

    Capture the number of users connected

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
+const numOfUsers = [];
 
-server.listen(port, () => {
+server.listen(port, function() {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('user connected');
-
+io.on('connection', function(socket) {
+  numOfUsers.push(socket);
+  console.log('Users connected: %s', numOfUsers.length);
   socket.on('sendMessage', function(msg) {
     console.log(`Message on index.js: ${msg}`);
     io.emit('appendMessage', msg);
   });
 
   socket.on('disconnect', function() {
-    console.log('Disconnect function: User disconnected');
+    numOfUsers.pop(socket);
+    console.log('User disconnected: %s Users connected', numOfUsers.length);
     io.emit('appendMessage', 'User disconnected');
   });
   
