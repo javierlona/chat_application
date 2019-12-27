eb2b2f7a9761f4a8cb4516cf90de09981a814f21
Javier Lona <javierlona1@gmail.com>
2019-12-27 17:54:22 -0600

    Dynamically show logged in users

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const port = 3000;
 const numOfUsers = [];
+const loggedInUsers = [];
 
 server.listen(port, function() {
   console.log(`Server is running on port ${port}`);
@@ -23,12 +24,22 @@ io.on('connection', function(socket) {
 
   socket.on('add user', function(username) {
     console.log(`Username in index.js: ${username}`);
+    socket.username = username;
+    loggedInUsers.push(socket.username);
+    io.emit('user joined', {
+      username: socket.username,
+    })
     io.emit('appendMessage', `${username} joined the chat.`);
   });
 
   socket.on('disconnect', function() {
     numOfUsers.pop(socket);
     console.log('User disconnected: %s Users connected', numOfUsers.length);
+    console.log(loggedInUsers.indexOf(socket.username));
+    io.emit('user left', {
+      username: socket.username,
+
+    });
     io.emit('appendMessage', 'User disconnected');
   });
   
